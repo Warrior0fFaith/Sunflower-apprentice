517aa06e79e802c961ea7f4cdf7ba6ccfc147342
Warrior0fFaith <39077314+Warrior0fFaith@users.noreply.github.com>
2018-05-07 22:54:59 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+require('events').EventEmitter.defaultMaxListeners = Infinity;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(config.defaultActivity);
+});
+
+client.on('message', msg => {
+    const pref = msg.content.slice(0, 1);
+    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if (pref === config.prefix) {
+
+        if (command === 'ping') {
+            msg.channel.send("Pong!");
+        } else {
+            return;
+        }
+    } else {
+        return;
+    }
+});
+
+client.login(config.token);
