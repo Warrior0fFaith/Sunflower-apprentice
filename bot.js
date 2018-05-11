1f73cecd35e9beec656938665d551e4adf503ab6
Warrior0fFaith <39077314+Warrior0fFaith@users.noreply.github.com>
2018-05-10 21:55:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,13 +24,13 @@ client.on('message', msg => {
             msg.channel.send(":sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower:")
           
         } else if (command === 'hugkirbea') {
-            msg.channel.send("", {file: "<https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif"});
+            msg.channel.send("", {file: "<https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif>"});
           
         } else if (command === 'help') {
             msg.channel.send("Have no fear for Sunny is here!^-^")
         
         } else if (command === 'List') {
-          msg.channel.send("-Ping -sunshine -hugkirbea -help")
+            msg.channel.send("-Ping -sunshine -hugkirbea -help")
           
         } else {
             return;
