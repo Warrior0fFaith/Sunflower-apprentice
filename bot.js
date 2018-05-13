00c1a7bf4d0378bf2dc3ac62c8b6844bd36c84fd
Warrior0fFaith <39077314+Warrior0fFaith@users.noreply.github.com>
2018-05-13 15:35:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ client.on('message', msg => {
 	} else if (command === 'flipflop') {
 	    msg.channel.send("", {file: "https://cdn.discordapp.com/attachments/444633646045986816/445123348385628160/flip_flop_slap.gif"});
 	
-	default: 
+	} default: {
 	    msg.channel.sendmessage("Invalid Command");
 		
         } else {
