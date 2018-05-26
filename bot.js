433ce7b960798e7b9e0fab34b55acf816aa88999
Warrior0fFaith <39077314+Warrior0fFaith@users.noreply.github.com>
2018-05-25 18:02:45 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ const music = new Music(client, {youtubeKey: process.env.ytkey, //Youtube Dev AP
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-	client.user.setPresence({game:{type: "WATCHING", name:"Warrior test commands"}});
+	client.user.setPresence({game:{type: "WATCHING", name:"Netflix while eating food"}});
 });
 
 client.on('message', msg => {
