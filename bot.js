44c784caaaffc18b781dc787d258833bc3c5f3d9
Warrior0fFaith <39077314+Warrior0fFaith@users.noreply.github.com>
2018-05-30 16:26:47 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', msg => {
     if (pref === config.prefix) {
 
         if (command === 'ping') {
-            msg.channel.send("**Ponged!**", {file: "https://uproxx.files.wordpress.com/2014/01/anime.gif"});
+            msg.channel.send('Pong! Your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
 
         } else if (command === 'sunshine') {
             msg.channel.send(":sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower::sunflower:")
